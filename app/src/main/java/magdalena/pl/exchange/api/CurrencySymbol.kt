1b36a17a97f2459f2a912de9c23b62558125660d
Magdalena Dziesińska <magdalena.dziesinska@ialbatros.com>
2018-12-27 20:13:55 +0100

    Add Currency Symbol class

---
+++ app/src/main/java/magdalena/pl/exchange/api/CurrencySymbol.kt
@@ -0,0 +1,513 @@
+package magdalena.pl.exchange.api
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CurrencySymbol(
+    @SerializedName("AED")
+    @Expose
+    val aED: String,
+    @SerializedName("AFN")
+    @Expose
+    val aFN: String,
+    @SerializedName("ALL")
+    @Expose
+    val aLL: String,
+    @SerializedName("AMD")
+    @Expose
+    val aMD: String,
+    @SerializedName("ANG")
+    @Expose
+    val aNG: String,
+    @SerializedName("AOA")
+    @Expose
+    val aOA: String,
+    @SerializedName("ARS")
+    @Expose
+    val aRS: String,
+    @SerializedName("AUD")
+    @Expose
+    val aUD: String,
+    @SerializedName("AWG")
+    @Expose
+    val aWG: String,
+    @SerializedName("AZN")
+    @Expose
+    val aZN: String,
+    @SerializedName("BAM")
+    @Expose
+    val bAM: String,
+    @SerializedName("BBD")
+    @Expose
+    val bBD: String,
+    @SerializedName("BDT")
+    @Expose
+    val bDT: String,
+    @SerializedName("BGN")
+    @Expose
+    val bGN: String,
+    @SerializedName("BHD")
+    @Expose
+    val bHD: String,
+    @SerializedName("BIF")
+    @Expose
+    val bIF: String,
+    @SerializedName("BMD")
+    @Expose
+    val bMD: String,
+    @SerializedName("BND")
+    @Expose
+    val bND: String,
+    @SerializedName("BOB")
+    @Expose
+    val bOB: String,
+    @SerializedName("BRL")
+    @Expose
+    val bRL: String,
+    @SerializedName("BSD")
+    @Expose
+    val bSD: String,
+    @SerializedName("BTC")
+    @Expose
+    val bTC: String,
+    @SerializedName("BTN")
+    @Expose
+    val bTN: String,
+    @SerializedName("BWP")
+    @Expose
+    val bWP: String,
+    @SerializedName("BYN")
+    @Expose
+    val bYN: String,
+    @SerializedName("BYR")
+    @Expose
+    val bYR: String,
+    @SerializedName("BZD")
+    @Expose
+    val bZD: String,
+    @SerializedName("CAD")
+    @Expose
+    val cAD: String,
+    @SerializedName("CDF")
+    @Expose
+    val cDFString: String,
+    @SerializedName("CHF")
+    @Expose
+    val cHF: String,
+    @SerializedName("CLF")
+    @Expose
+    val cLF: String,
+    @SerializedName("CLP")
+    @Expose
+    val cLP: String,
+    @SerializedName("CNY")
+    @Expose
+    val cNY: String,
+    @SerializedName("COP")
+    @Expose
+    val cOP: String,
+    @SerializedName("CRC")
+    @Expose
+    val cRC: String,
+    @SerializedName("CUC")
+    @Expose
+    val cUC: String,
+    @SerializedName("CUP")
+    @Expose
+    val cUP: String,
+    @SerializedName("CVE")
+    @Expose
+    val cVE: String,
+    @SerializedName("CZK")
+    @Expose
+    val cZK: String,
+    @SerializedName("DJF")
+    @Expose
+    val dJF: String,
+    @SerializedName("DKK")
+    @Expose
+    val dKK: String,
+    @SerializedName("DOP")
+    @Expose
+    val dOP: String,
+    @SerializedName("DZD")
+    @Expose
+    val dZD: String,
+    @SerializedName("EGP")
+    @Expose
+    val eGP: String,
+    @SerializedName("ERN")
+    @Expose
+    val eRN: String,
+    @SerializedName("ETB")
+    @Expose
+    val eTB: String,
+    @SerializedName("EUR")
+    @Expose
+    val eUR: String,
+    @SerializedName("FJD")
+    @Expose
+    val fJD: String,
+    @SerializedName("FKP")
+    @Expose
+    val fKP: String,
+    @SerializedName("GBP")
+    @Expose
+    val gBP: String,
+    @SerializedName("GEL")
+    @Expose
+    val gEL: String,
+    @SerializedName("GGP")
+    @Expose
+    val gGP: String,
+    @SerializedName("GHS")
+    @Expose
+    val gHS: String,
+    @SerializedName("GIP")
+    @Expose
+    val gIP: String,
+    @SerializedName("GMD")
+    @Expose
+    val gMD: String,
+    @SerializedName("GNF")
+    @Expose
+    val gNF: String,
+    @SerializedName("GTQ")
+    @Expose
+    val gTQ: String,
+    @SerializedName("GYD")
+    @Expose
+    val gYD: String,
+    @SerializedName("HKD")
+    @Expose
+    val hKD: String,
+    @SerializedName("HNL")
+    @Expose
+    val hNL: String,
+    @SerializedName("HRK")
+    @Expose
+    val hRK: String,
+    @SerializedName("HTG")
+    @Expose
+    val hTG: String,
+    @SerializedName("HUF")
+    @Expose
+    val hUF: String,
+    @SerializedName("IDR")
+    @Expose
+    val iDR: String,
+    @SerializedName("ILS")
+    @Expose
+    val iLS: String,
+    @SerializedName("IMP")
+    @Expose
+    val iMP: String,
+    @SerializedName("INR")
+    @Expose
+    val iNR: String,
+    @SerializedName("IQD")
+    @Expose
+    val iQD: String,
+    @SerializedName("IRR")
+    @Expose
+    val iRR: String,
+    @SerializedName("ISK")
+    @Expose
+    val iSK: String,
+    @SerializedName("JEP")
+    @Expose
+    val jEP: String,
+    @SerializedName("JMD")
+    @Expose
+    val jMD: String,
+    @SerializedName("JOD")
+    @Expose
+    val jOD: String,
+    @SerializedName("JPY")
+    @Expose
+    val jPY: String,
+    @SerializedName("KES")
+    @Expose
+    val kES: String,
+    @SerializedName("KGS")
+    @Expose
+    val kGS: String,
+    @SerializedName("KHR")
+    @Expose
+    val kHR: String,
+    @SerializedName("KMF")
+    @Expose
+    val kMF: String,
+    @SerializedName("KPW")
+    @Expose
+    val kPW: String,
+    @SerializedName("KRW")
+    @Expose
+    val kRW: String,
+    @SerializedName("KWD")
+    @Expose
+    val kWD: String,
+    @SerializedName("KYD")
+    @Expose
+    val kYD: String,
+    @SerializedName("KZT")
+    @Expose
+    val kZT: String,
+    @SerializedName("LAK")
+    @Expose
+    val lAK: String,
+    @SerializedName("LBP")
+    @Expose
+    val lBP: String,
+    @SerializedName("LKR")
+    @Expose
+    val lKR: String,
+    @SerializedName("LRD")
+    @Expose
+    val lRD: String,
+
+    @SerializedName("LSL")
+    @Expose
+    val lSL: String,
+    @SerializedName("LTL")
+    @Expose
+    val lTL: String,
+    @SerializedName("LVL")
+    @Expose
+    val lVL: String,
+    @SerializedName("LYD")
+    @Expose
+    val lYD: String,
+    @SerializedName("MAD")
+    @Expose
+    val mAD: String,
+    @SerializedName("MDL")
+    @Expose
+    val mDL: String,
+    @SerializedName("MGA")
+    @Expose
+    val mGA: String,
+    @SerializedName("MKD")
+    @Expose
+    val mKD: String,
+    @SerializedName("MMK")
+    @Expose
+    val mMK: String,
+    @SerializedName("MNT")
+    @Expose
+    val mNT: String,
+    @SerializedName("MOP")
+    @Expose
+    val mOP: String,
+    @SerializedName("MRO")
+    @Expose
+    val mRO: String,
+    @SerializedName("MUR")
+    @Expose
+    val mUR: String,
+    @SerializedName("MVR")
+    @Expose
+    val mVR: String,
+    @SerializedName("MWK")
+    @Expose
+    val mWK: String,
+    @SerializedName("MXN")
+    @Expose
+    val mXN: String,
+    @SerializedName("MYR")
+    @Expose
+    val mYR: String,
+    @SerializedName("MZN")
+    @Expose
+    val mZN: String,
+    @SerializedName("NAD")
+    @Expose
+    val nAD: String,
+    @SerializedName("NGN")
+    @Expose
+    val nGN: String,
+    @SerializedName("NIO")
+    @Expose
+    val nIO: String,
+    @SerializedName("NOK")
+    @Expose
+    val nOK: String,
+    @SerializedName("NPR")
+    @Expose
+    val nPR: String,
+    @SerializedName("NZD")
+    @Expose
+    val nZD: String,
+    @SerializedName("OMR")
+    @Expose
+    val oMR: String,
+    @SerializedName("PAB")
+    @Expose
+    val pAB: String,
+    @SerializedName("PEN")
+    @Expose
+    val pEN: String,
+    @SerializedName("PGK")
+    @Expose
+    val pGK: String,
+    @SerializedName("PHP")
+    @Expose
+    val pHP: String,
+    @SerializedName("PKR")
+    @Expose
+    val pKR: String,
+    @SerializedName("PLN")
+    @Expose
+    val pLN: String,
+    @SerializedName("PYG")
+    @Expose
+    val pYG: String,
+
+    @SerializedName("QAR")
+    @Expose
+    val qAR: String,
+    @SerializedName("RON")
+    @Expose
+    val rON: String,
+    @SerializedName("RSD")
+    @Expose
+    val rSD: String,
+    @SerializedName("RUB")
+    @Expose
+    val rUB: String,
+    @SerializedName("RWF")
+    @Expose
+    val rWF: String,
+    @SerializedName("SAR")
+    @Expose
+    val sAR: String,
+    @SerializedName("SBD")
+    @Expose
+    val sBD: String,
+    @SerializedName("SCR")
+    @Expose
+    val sCR: String,
+    @SerializedName("SDG")
+    @Expose
+    val sDG: String,
+    @SerializedName("SEK")
+    @Expose
+    val sEK: String,
+    @SerializedName("SGD")
+    @Expose
+    val sGD: String,
+    @SerializedName("SHP")
+    @Expose
+    val sHP: String,
+    @SerializedName("SLL")
+    @Expose
+    val sLL: String,
+    @SerializedName("SOS")
+    @Expose
+    val sOS: String,
+    @SerializedName("SRD")
+    @Expose
+    val sRD: String,
+    @SerializedName("STD")
+    @Expose
+    val sTD: String,
+    @SerializedName("SVC")
+    @Expose
+    val sVC: String,
+    @SerializedName("SYP")
+    @Expose
+    val sYP: String,
+    @SerializedName("SZL")
+    @Expose
+    val sZL: String,
+    @SerializedName("THB")
+    @Expose
+    val tHB: String,
+    @SerializedName("TJS")
+    @Expose
+    val tJS: String,
+    @SerializedName("TMT")
+    @Expose
+    val tMT: String,
+    @SerializedName("TND")
+    @Expose
+    val tND: String,
+    @SerializedName("TOP")
+    @Expose
+    val top: String,
+    @SerializedName("TRY")
+    @Expose
+    val tRY: String,
+    @SerializedName("TTD")
+    @Expose
+    val tTD: String,
+    @SerializedName("TWD")
+    @Expose
+    val tWD: String,
+    @SerializedName("TZS")
+    @Expose
+    val tZS: String,
+    @SerializedName("UAH")
+    @Expose
+    val uAH: String,
+    @SerializedName("UGX")
+    @Expose
+    val uGX: String,
+    @SerializedName("USD")
+    @Expose
+    val uSD: String,
+    @SerializedName("UYU")
+    @Expose
+    val uYU: String,
+    @SerializedName("UZS")
+    @Expose
+    val uZS: String,
+    @SerializedName("VEF")
+    @Expose
+    val vEF: String,
+    @SerializedName("VND")
+    @Expose
+    val vND: String,
+    @SerializedName("VUV")
+    @Expose
+    val vUV: String,
+    @SerializedName("WST")
+    @Expose
+    val wST: String,
+    @SerializedName("XAF")
+    @Expose
+    val xAF: String,
+    @SerializedName("XAG")
+    @Expose
+    val xAG: String,
+    @SerializedName("XAU")
+    @Expose
+    val xAU: String,
+    @SerializedName("XCD")
+    @Expose
+    val xCD: String,
+    @SerializedName("XDR")
+    @Expose
+    val xDR: String,
+    @SerializedName("XOF")
+    @Expose
+    val xOF: String,
+    @SerializedName("XPF")
+    @Expose
+    val xPF: String,
+    @SerializedName("YER")
+    @Expose
+    val yER: String,
+    @SerializedName("ZAR")
+    @Expose
+    val zAR: String,
+    @SerializedName("ZMK")
+    @Expose
+    val zMK: String,
+    @SerializedName("ZMW")
+    @Expose
+    val zMW: String,
+    @SerializedName("ZWL")
+    @Expose
+    val zWL: String
+)
